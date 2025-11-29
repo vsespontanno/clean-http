e934e11166efd481a10ce08a11ff5a765c4fe1a7
thomas <moryakovpirat@gmail.com>
2025-11-29 20:11:45 +0300

    net is done; next http

--- src/httpServer/extclib/net.c
+++ src/httpServer/extclib/net.c
@@ -11,6 +11,10 @@
 
 #if defined(__linux__) || defined(_WIN32) || defined(WIN32) || defined(__APPLE__)
 
+#include <stddef.h>
+#include <stdlib.h>
+#include <stdint.h>
+
 typedef enum error_t {
     WINSOCK_ERR = -1,
     SOCKET_ERR = -2,
@@ -18,12 +22,16 @@ typedef enum error_t {
     PARSE_ERR = -4,
     BIND_ERR = -5,
     LISTEN_ERR = -6,
+    CONNECT_ERR = -7,
 } error_t;
 
 #include "net.h"
 
+
+static int8_t _parse_address(char *address, char *ipv4, char *port);
+
 extern int listen_net(char *address){
-#ifdef _WIN32 || WIN32
+#if defined(__WIN32) || defined(WIN32) 
     WSDATA wsa;
     if (WSAStartuo(MAKEWORD(2, 2), &wsa) != 0) {
         return WINSOCK_ERR;
@@ -59,14 +67,67 @@ extern int accept_net(int listener){
 }
 
 extern int connect_net(char *address){
+#if defined(__WIN32) || defined(WIN32) 
+    WSDATA wsa;
+    if (WSAStartuo(MAKEWORD(2, 2), &wsa) != 0) {
+        return WINSOCK_ERR;
+    }
+#endif
+    int conn = socket(AF_INET, SOCK_STREAM, 0);
+    if (conn < 0) {
+        return SOCKET_ERR;
+    }
+    char ipv4[16];
+    char port[6];
+    if (_parse_address(address, ipv4, port) != 0) {
+        return PARSE_ERR;
+    }
+    struct sockaddr_in addr;
+    addr.sin_family = AF_INET;
+    addr.sin_port = htons(atoi(port));
+    addr.sin_addr.s_addr = inet_addr(ipv4);
+    if (connect(conn, (struct sockaddr*)&addr, sizeof(addr)) != 0) {
+        return CONNECT_ERR;
+    }
+    return conn;
 }
 
 extern int close_net(int conn){
+#if defined(__linux__)  ||  defined(__APPLE__)
+    return close(conn);
+#elif defined(__WIN32) || defined(WIN32) 
+    return closesocket(conn);
+#endif
 }
 
 extern int send_net(int conn, char *buffer, size_t size){
-}
-extern int recv_net(int conn, char *buffer, size_t size){
+    return send(conn, buffer, (int)size, 0);
 }
 
-#endif
+extern int recv_net(int conn, char *buffer, size_t size){
+    return recv(conn, buffer, (int)size, 0);
+}
+
+static int8_t _parse_address(char *address, char *ipv4, char *port) {
+    size_t i = 0, j = 0;
+    for (; address[i] != ':'; i++) {
+        if (address[i] == '\0') {
+            return 1;
+        }
+        if (i >= 15) {
+            return 2;
+        }
+        ipv4[i] = address[i];
+    }
+    ipv4[i] = '\0';
+    for (i += 1 ; address[i] != '\0'; ++i, ++j)  {
+        if (j >= 5) {
+            return 3;
+        }
+        port[j] = address[i];
+    }
+    port[j] = '\0';
+    return 0;
+}
+
+#endif /*defined(__linux__) || defined(_WIN32) || defined(WIN32) || defined(__APPLE__)*/
